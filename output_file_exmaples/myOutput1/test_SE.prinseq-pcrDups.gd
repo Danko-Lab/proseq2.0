1e80091df5c0cb26c7d82a091405264b89ad0399
shaopei <sc2457@cornell.edu>
2018-08-21 12:35:11 -0400

    add example files

---
+++ output_file_exmaples/myOutput1/test_SE.prinseq-pcrDups.gd
@@ -0,0 +1,10 @@
+Input and filter stats:
+	Input sequences: 6,425
+	Input bases: 247,838
+	Input mean length: 38.57
+	Good sequences: 6,425 (100.00%)
+	Good bases: 247,838
+	Good mean length: 38.57
+	Bad sequences: 0 (0.00%)
+	Sequences filtered by specified parameters:
+	none
